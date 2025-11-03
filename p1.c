6abfccb7107b1dce0911e7c5bff2cd51b48b34db
Mohammad Saif <azharisaif09@gmail.com>
2025-11-03 14:24:02 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -6,13 +6,13 @@ int main() {
     printf("Enter a 5-digit number: ");
     scanf("%d", &num);
 
-    // Process each digit from right to left
+    // Process har digit ka right to left
     while (num > 0) {
         digit = num % 10;     // Get last digit
-        digit = (digit + 1) % 10; // 1 add kro, wrap around if 9 → 0
+        digit = (digit + 1) % 10; // 1 add kro, if 9 to 0
         new_num = new_num + digit * place;
         place = place * 10;
-        num = num / 10;       // Remove last digit
+        num = num / 10;       // Remove krega last digit
     }
 
     printf("New number after adding 1 to each digit = %d\n", new_num);
